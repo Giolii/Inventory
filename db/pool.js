5c238e3f065460a3a86c45812617bc66c49076a0
Gi <160989051+Giolii@users.noreply.github.com>
2024-12-12 09:26:59 -0800

    Deleted console.log debug

--- db/pool.js
+++ db/pool.js
@@ -1,8 +1,6 @@
 require("dotenv").config();
 const { Pool } = require("pg");
 
-console.log("DATABASE_URL:", process.env.DATABASE_URL); // Add this to check
-
 module.exports = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
