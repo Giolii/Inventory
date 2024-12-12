3ea2d219d28498ef3b927214d5f0180225ab08bc
Gi <160989051+Giolii@users.noreply.github.com>
2024-12-12 09:05:15 -0800

    Corrected the pool for online db

--- db/pool.js
+++ db/pool.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const { Pool } = require("pg");
 
+console.log("DATABASE_URL:", process.env.DATABASE_URL); // Add this to check
+
 module.exports = new Pool({
   connectionString:
     "postgresql://postgres:NEftnyyjALptQJZyxElGOPuzlIUckcdN@postgres.railway.internal:5432/railway",
